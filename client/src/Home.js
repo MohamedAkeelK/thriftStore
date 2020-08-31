9d946f8f14770d02384565c6ecf379f02a92d3cf
kemvlem <asitimbaykemlem@gmail.com>
2020-08-31 00:34:22 -0400

    nav working

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <img src={logo} className="App-logo" alt="logo" />
+       
       </div>
     );
   }
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "./logo.png";
+import { Input,Icon } from 'semantic-ui-react'
 
 const NavBar = (props) => {
   return (
     <div>
-      <Link to="/">
-        <h1 className="logo">home</h1>
-      </Link>
+      <div className='blackBar'>
+        Free Shipping on Orders over $50
+      </div>
+      <header className="header">
+      <ul className="main-nav">
+      <li> <Link to="/">
+      <Icon name='search' />
+       </Link></li>
+       
+          <li> <Link to="/">
+          <img className='logoimg' src={logo}></img>
+       </Link></li>
+     
+       <li> <Link to="/">
+       <Icon name='shopping bag' />
+       </Link></li>
+       <li> <Link to="/">
+       Sign in
+       </Link></li>
+      </ul>
+	</header>
+     
     </div>
   );
 };
