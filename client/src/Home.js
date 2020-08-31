24297f7dd36ac7ea5d76a8586bc3fda56d026d41
Maricela Patrickios <mreyes777@gmail.com>
2020-08-31 15:04:14 -0400

    Removed the .gitIgnore 2 file and fixed the merge conflicts

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,20 +1,8 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-
-
-class Home extends Component {
-  render() {
-    return (
-      <div>
-       
-      </div>
-    );
-=======
 
 class Home extends Component {
   render() {
     return <div></div>;
->>>>>>> master
   }
 }
 
