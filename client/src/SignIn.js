dd66c62ec9884936817c9ed9dd3124c1519514d3
MohamedAkeelK <67453119+MohamedAkeelK@users.noreply.github.com>
2020-09-03 16:05:27 -0400

    Sign in (#13)
    * created footer
    
    * commiting
    
    * added banner
    
    * fixe banner and signin page
    
    * added padding

--- client/src/SignIn.js
+++ client/src/SignIn.js
@@ -1,1 +1,26 @@
 // here  you need to create a react component for the signin page.
+import React from 'react'
+
+export default function SignIn() {
+    return (
+        <div>
+            <div class="login-page">
+  <div class="form">
+    <form class="register-form">
+      <input type="text" placeholder="user"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p class="message">Already registered? <a href="#">Sign In</a></p>
+    </form>
+    <form class="login-form">
+      <input type="text" placeholder="Email"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+      <p class="message">Not registered? <a href="#">Create an account</a></p>
+    </form>
+  </div>
+</div>
+        </div>
+    )
+}
