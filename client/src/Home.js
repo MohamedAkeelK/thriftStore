64877e302aeaad66c27f9690caf531d99e030972
unknown <Mohamedakeelkhan@gmail.com>
2020-09-03 12:57:04 -0400

    created footer

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,21 +1,8 @@
 import React, { Component } from "react";
 
-<<<<<<< HEAD
-
-
-class Home extends Component { 
-  render() {
-    return (
-      <div>
-       
-      </div>
-    );
-    }
-=======
 class Home extends Component {
   render() {
     return <div></div>;
   }
->>>>>>> a846c81367574a4502d6321db19deed9777cf5a3
 }
 export default Home;
