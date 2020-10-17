5ce68506d81052d9581043607d88830409f01183
unknown <Mohamedakeelkhan@gmail.com>
2020-10-17 02:02:53 -0400

    first commit

--- client/src/HomeContent.js
+++ client/src/HomeContent.js
@@ -3,7 +3,25 @@ import React from "react";
 const Content = (props) => {
   return (
     <div>
-      <h1>Content</h1>
+      <div className="homeContent">
+        <h1 className="contentHeading">About Us</h1>
+        <p className="content">
+          Hi, this is a project developed by a group of devlopers named sdknssi
+          , vnsjvn , sdvnsnvjs ,sdfnjsd , sdksdndskn. This is a full stack
+          application created using react, node, express, javascript and
+          postgres sql , this project has thought us the ins and outs of
+          creating a full stack application. Hi, this is a project developed by
+          a group of devlopers named sdknssi , vnsjvn , sdvnsnvjs ,sdfnjsd ,
+          sdksdndskn. This is a full stack application created using react,
+          node, express, javascript and postgres sql , this project has thought
+          us the ins and outs of creating a full stack application. Hi, this is
+          a project developed by a group of devlopers named sdknssi , vnsjvn ,
+          sdvnsnvjs ,sdfnjsd , sdksdndskn. This is a full stack application
+          created using react, node, express, javascript and postgres sql , this
+          project has thought us the ins and outs of creating a full stack
+          application.
+        </p>
+      </div>
     </div>
   );
 };
--- client/src/SideBar.js
+++ client/src/SideBar.js
@@ -1,33 +1,83 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import logo from "./logo.png";
+
+// const SideBar = (props) => {
+//   return (
+//     <div className="sidenav">
+//       {/* <Switch>
+//         <Route
+//           exact
+//           path="/tees"
+//           render={(routerProps) => <Tops {...routerProps} />}
+//         />
+//         <Route
+//           exact
+//           path="/denim"
+//           render={(routerProps) => <Denim {...routerProps} />}
+//         />
+//         <Route
+//           exact
+//           path="/jackets"
+//           render={(routerProps) => <Jackets {...routerProps} />}
+//         />
+//       </Switch>  */}
+//       {/* <div class="sidenav">
+//         <a href="#">About</a>
+//         <a href="#">Tees/Tops</a>
+//         <a href="#">Jackets</a>
+//         <a href="#">Denims</a>
+//         <a href="#">Bags</a>
+//       </div> */}
+
+//       <ul className="side-nav-ul">
+//         <li>
+//           {" "}
+//           <Link to="/">
+//             <p>Jackets</p>
+//             <img className="logoimg" src={logo}></img>
+//           </Link>
+//         </li>
+
+//         <li>
+//           {" "}
+//           <Link to="/Denims">Denims</Link>
+//         </li>
+
+//         <li>
+//           {" "}
+//           <Link to="/bags"></Link>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// };
+// export default SideBar;
+
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SideBar = (props) => {
   return (
-    <div className="sidenav">
-      {/* <Switch>
-        <Route
-          exact
-          path="/tees"
-          render={(routerProps) => <Tops {...routerProps} />}
-        />
-        <Route
-          exact
-          path="/denim"
-          render={(routerProps) => <Denim {...routerProps} />}
-        />
-        <Route
-          exact
-          path="/jackets"
-          render={(routerProps) => <Jackets {...routerProps} />}
-        />
-      </Switch>  */}
-      <div class="sidenav">
-        <a href="#">About</a>
-        <a href="#">Tees/Tops</a>
-        <a href="#">Jackets</a>
-        <a href="#">Denims</a>
-        <a href="#">Bags</a>
-      </div>
+    <div>
+      <ul className="side-nav">
+        <li>
+          {" "}
+          <Link to="/Jackets">jackets</Link>
+        </li>
+
+        <li>
+          {" "}
+          <Link to="/Denim">Denim</Link>
+        </li>
+
+        <li>
+          {" "}
+          <Link to="/Bags">Bags</Link>
+        </li>
+      </ul>
     </div>
   );
 };
+
 export default SideBar;
